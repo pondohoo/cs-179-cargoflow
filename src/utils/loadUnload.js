6b22b414b3ea87692b82e26581f3df74b5b7931e
theo <theobfernandez@gmail.com>
2024-11-07 20:27:01 -0800

    add placeholder AI search algorithms

---
+++ src/utils/loadUnload.js
@@ -0,0 +1,9 @@
+const loadUnload = (manifest) => {
+	const listOfMoves = [
+		[(6, 2), (8, 2)],
+		[(6, 1), (1, 2)],
+	]; // move (6,2) to (8,2) and (6,1) to (1,2)
+	return listOfMoves;
+};
+
+export default loadUnload;
