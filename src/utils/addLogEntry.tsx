072399be35f2d223661fb9acdeda954a1d2d83ca
Desmond Chi <desmonddchi@gmail.com>
2024-11-26 14:39:27 -0500

    [Logs] Create "Add log entry" (comment) button.
    The comment button is displayed on all views and pressing on it will
    prompt a text entry for the user to type in any comments as a log entry.
    The backend for storing the log entries in an IndexedDB database is set
    up.
    
    Next steps are to:
    - Add date and time for log entries.
    - Create download log button: parse through IndexedDB record and output
      into a `txt`.

---
+++ src/utils/addLogEntry.tsx
@@ -0,0 +1,53 @@
+// Adds a log entry. Each log entry is a string and the log entires are stored in a single stringlist in a single record in an IndexedDB database.
+export default function addLogEntry(inputStr: string) {
+  const currYear = new Date().getFullYear();
+  const databaseName = "cargoflowDatabase";
+  const storeName: string = "cargoflowLogs" + currYear;
+
+  const dbRequest: IDBOpenDBRequest = indexedDB.open(databaseName, 1);
+
+  dbRequest.onupgradeneeded = (event: any) => {
+    const db: IDBDatabase = event.target.result;
+    const objectStore: IDBObjectStore = db.createObjectStore(storeName, {
+      keyPath: "id",
+    });
+  };
+
+  dbRequest.onsuccess = (event: any) => {
+    const db: IDBDatabase = event.target.result;
+    const transaction: IDBTransaction = db.transaction(storeName, "readwrite");
+    const objectStore: IDBObjectStore = transaction.objectStore(storeName);
+
+    const getRequest: IDBRequest = objectStore.get(storeName);
+
+    getRequest.onsuccess = (event: any) => {
+      const result = event.target.result;
+
+      // Update the stringlist in current record if the record already exists.
+      if (result !== undefined) {
+        result.stringlist.push(inputStr);
+        const putRequest: IDBRequest = objectStore.put(result);
+
+        putRequest.onerror = (event: any) => {
+          console.log("Error adding log entry and updating current record.");
+        };
+      } else {
+        // Create the record for which the stringlist will be stored.
+        const data = {
+          id: storeName,
+          stringlist: [inputStr],
+        };
+
+        objectStore.add(data);
+      }
+    };
+
+    getRequest.onerror = (event: any) => {
+      console.error("Error getting objectStore request.");
+    };
+  };
+
+  dbRequest.onerror = (event: Event) => {
+    console.error("Error opening IndexedDB database.");
+  };
+}
