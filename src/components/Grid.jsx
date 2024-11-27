f780f03dbef9ad95346692f4a9be5c89fc7894a5
theo <theobfernandez@gmail.com>
2024-11-26 21:58:28 -0300

    add another row

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,7 +5,7 @@ const Grid = ({ manifest, currentStep }) => {
 	return (
 		<div className="flex justify-center items-center">
 			<div className="flex flex-col-reverse">
-				{Array.from({ length: 8 }).map((_, i) => (
+				{Array.from({ length: manifest.length / 12 }).map((_, i) => (
 					<div key={i} className="flex">
 						{manifest.slice(i * 12, (i + 1) * 12).map((entry, index) => (
 							<div key={index}>
--- src/components/UploadManifest.jsx
+++ src/components/UploadManifest.jsx
@@ -15,11 +15,12 @@ const UploadManifest = ({ setManifest, setShipName }) => {
 	const formatManifest = (content) => {
 		const lines = content.split("\n");
 		const result = [];
+
 		lines.forEach((line) => {
 			const [rowString, colString, weightString, nameString] = line.split(",");
 
 			const row = parseInt(rowString.trim().slice(1), 10);
-			const col = parseInt(colString.trim().slice(0,-1), 10);
+			const col = parseInt(colString.trim().slice(0, -1), 10);
 			const name = nameString.trim();
 			const weight = parseInt(weightString.trim().slice(1, -1));
 
@@ -32,6 +33,15 @@ const UploadManifest = ({ setManifest, setShipName }) => {
 				name,
 			});
 		});
+		for (let i = 0; i < 12; i++) {
+			result.push({
+				row: 9,
+				col: i + 1,
+				weight: 0,
+				name: "UNUSED",
+			});
+		}
+		console.log(result)
 		return result;
 	};
 	const handleInput = (e) => {
