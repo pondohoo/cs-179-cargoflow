05e231355ff21ecf6193838d28b23f3fed789e76
theo <theobfernandez@gmail.com>
2024-11-27 21:27:12 -0300

    add popup to grid squares that display names of containers

--- src/components/GridSquare.jsx
+++ src/components/GridSquare.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 const GridSquare = ({ entry, currentStep }) => {
+	const [showPopup, setShowPopup] = useState(false);
+
+	const handleMouseEnter = () => {
+		if (entry.name != "NAN" && entry.name != "UNUSED")
+		{
+			setShowPopup(true);
+		}
+	};
+
+	const handleMouseLeave = () => {
+		setShowPopup(false);
+	};
 	const getColor = () => {
 		if (currentStep) {
 			if (
@@ -32,7 +46,20 @@ const GridSquare = ({ entry, currentStep }) => {
 			return "bg-white";
 		} else return "bg-ibm-pink";
 	};
-	return <div className={`w-8 h-8 ${getColor()} border border-black`}></div>;
+	return (
+		<div
+			className={`w-8 h-8 ${getColor()} border border-black relative`}
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+		>
+			{showPopup && (
+				<div className="absolute z-10 bg-white text-black border border-black p-1 text-xs">
+					{`Name: ${entry.name}, Coordinates: (${entry.row}, ${entry.col})`}
+				</div>
+			)}
+		</div>
+	);
 };
+
 
 export default GridSquare;
