94be615ee4bb8140b65a4517bdbd73da68ab1567
theo <theobfernandez@gmail.com>
2024-12-06 13:04:30 -0800

    set up submission website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
 /* eslint-disable new-cap */
 /* eslint-disable camelcase */
 import "./globals.css";
-import AddLogEntry from "@/components/AddLogEntry"
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={`bg-ibm-blue overscroll-none flex justify-center items-center flex-col min-h-screen`}>
-				<AddLogEntry />
 				{children}
 			</body>
 		</html>
