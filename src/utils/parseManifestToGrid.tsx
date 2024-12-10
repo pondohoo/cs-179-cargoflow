db5492f6dd6bc736ce51f03d2bbaa66c2e9afe84
Desmond Chi <desmonddchi@gmail.com>
2024-12-09 21:12:38 -0800

    Add util function to parse manifest into a 2D grid. (#28)

--- src/utils/parseManifestToGrid.tsx
+++ src/utils/parseManifestToGrid.tsx
@@ -1,3 +1,22 @@
-export default function parseManifestToGrid() {
-  
+export interface ManifestEntry {
+  row: number;
+  col: number;
+  weight: number;
+  name: string;
+}
+
+export default function parseManifestToGrid(
+  manifest: ManifestEntry[]
+): ManifestEntry[][] {
+  let grid: ManifestEntry[][] = [];
+  // First 8 x 12 entries in manifest are grid items starting from bottom row to top.
+  for (let r = 0; r < 8; r++) {
+    let row: ManifestEntry[] = [];
+    for (let c = 0; c < 12; c++) {
+      row.push(manifest[r * 12 + c]);
+    }
+    grid.push(row);
+  }
+
+  return grid;
 }
