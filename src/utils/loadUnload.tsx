f2abc83ec805d2b82695bcda9d987b0d39db9527
Desmond Chi <desmonddchi@gmail.com>
2024-12-09 21:10:35 -0800

    Add util function to parse manifest into a 2D grid.

---
+++ src/utils/loadUnload.tsx
@@ -0,0 +1,28 @@
+import parseManifestToGrid from "@/utils/parseManifestToGrid";
+import { ManifestEntry } from "@/utils/parseManifestToGrid";
+
+const loadUnload = (manifest, containersToLoad, containersToUnload) => {
+  let grid: ManifestEntry[][] = parseManifestToGrid(manifest);
+
+  const listOfMoves = [
+    [
+      [2, 6],
+      [2, 1],
+    ],
+    [
+      [1, 6],
+      [2, 12],
+    ],
+    [
+      [2, 5],
+      [2, 11],
+    ],
+    [
+      [1, 5],
+      [3, 11],
+    ],
+  ]; // move [2, 6] to [2, 1], then [1, 6] to [2, 12], etc.
+  return listOfMoves;
+};
+
+export default loadUnload;
