5f87f3dfcd427e0a55c2ed067ea035aca362a543
theo <theobfernandez@gmail.com>
2024-12-12 14:16:05 -0800

    start submission website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
 /* eslint-disable new-cap */
 /* eslint-disable camelcase */
 import "./globals.css";
-import AddLogEntry from "@/components/AddLogEntry";
-import DownloadLogs from "@/components/DownloadLogs"
-import Login from "@/components/Login";
 
 export default function RootLayout({ children }) {
   return (
@@ -11,12 +8,6 @@ export default function RootLayout({ children }) {
       <body
         className={`bg-ibm-blue overscroll-none flex justify-center items-center flex-col min-h-screen`}
       >
-        <div className="absolute bottom-4 right-4 flex space-x-4">
-					<DownloadLogs />
-          <AddLogEntry />
-          <Login />
-        </div>
-
         {children}
       </body>
     </html>
