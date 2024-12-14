9094cad22924e8da0a3a69dbeb839b54815dcee4
DannielK <dannykm003@gmail.com>
2024-12-13 23:33:44 -0800

    Updated submission page format

--- src/app/submission/page.js
+++ src/app/submission/page.js
@@ -2,119 +2,108 @@ const Submission = () => {
 	return (
 		<div className="text-black bg-blue-100 p-8 font-sans leading-relaxed">
 			<div className="flex gap-10">
-				<h1 className="text-2xl font-bold mb-2">Team CargoFlow Inc.</h1>
-				<p className="italic text-gray-600 mb-4">
+				<h1 className="mb-2">Team CargoFlow Inc.</h1>
+				<p className="italic text-gray-600 mb-4 ml-20">
 					(This webpage was last edited on 12-13-2024)
 				</p>
 			</div>
-
-			<h2 className="text-xl font-semibold mb-2">Team Members</h2>
-			<ul className="list-disc list-inside mb-4">
-				<li>
-					Jason Chau,{" "}
+			<ul className="list-disc list-inside mb-4 ml-20">
+				    Jason Chau,{" "}
 					<a href="mailto:jchau044@ucr.edu" className="text-blue-600 underline">
 						jchau044@ucr.edu
 					</a>
-				</li>
-				<li>
-					Danniel Kim,{" "}
+					<br></br>Danniel Kim,{" "}
 					<a href="mailto:dkim481@ucr.edu" className="text-blue-600 underline">
 						dkim481@ucr.edu
 					</a>
-				</li>
-				<li>
-					Gurjot Singh,{" "}
+					<br></br>Gurjot Singh,{" "}
 					<a href="mailto:gsing064@ucr.edu" className="text-blue-600 underline">
 						gsing064@ucr.edu
 					</a>
-				</li>
-				<li>
-					Desmond Chi,{" "}
+					<br></br>Desmond Chi,{" "}
 					<a href="mailto:dchi008@ucr.edu" className="text-blue-600 underline">
 						dchi008@ucr.edu
 					</a>
-				</li>
-				<li>
-					Theo Fernandez,{" "}
+					<br></br>Theo Fernandez,{" "}
 					<a href="mailto:tfern009@ucr.edu" className="text-blue-600 underline">
 						tfern009@ucr.edu
 					</a>
-				</li>
 			</ul>
-
-			<h2 className="text-xl font-semibold mb-2">Project Files</h2>
 			<ol className="list-decimal list-inside mb-4 space-y-2">
 				<li>
+					This file&nbsp;
 					<a
 						href="https://drive.google.com/file/d/1JL_BzXRfGbNrWAonA-HwpFXcyRX3kEVN/view?usp=sharing"
 						className="text-blue-600 underline"
 					>
 						<strong>CargoFlow.pdf</strong>
 					</a>{" "}
-					- Evidence of our efforts to plan our elicitation of requirements from
-					Mr. Keogh, and any notes taken during the elicitation.
+					contains evidence of our efforts to plan our elicitation of requirements from Mr. Keogh, and any notes taken during the elicitation.
 				</li>
 				<li className="">
+					This file&nbsp;
 					<a
 						href="https://drive.google.com/file/d/1FOM86qgRk5hVelKeKrLsziqeBVaV9P2Y/view?usp=sharing"
 						className="text-blue-600 underline"
 					>
-						<strong>CargoFlowFall2024_Attempt1.pdf</strong>
+						<strong>CargoFlowProjectPitchORIGINAL.pdf</strong>
 					</a>{" "}
-					- Our original pitch to Mr. Keogh, delivered on October 13th, 2024, at
+					contains our original pitch to Mr. Keogh, which was delivered on October 13th, 2024, at
 					9:00 AM.
 				</li>
 				<li>
+					This file&nbsp;
 					<a
 						href="https://drive.google.com/file/d/1iKqO5GUQ__ExUX9pW2Knd4O1utOK-q6q/view?usp=sharing"
 						className="text-blue-600 underline"
 					>
-						<strong>CargoFlowFall2024_Attempt1_Revised.pdf</strong>
+						<strong>CargoFlowProjectPitchCORRECTED.pdf</strong>
 					</a>{" "}
-					- A new version of the pitch with all corrections, amendments, and
-					deletions suggested by Mr. Keogh (and/or Dr. Keogh).
+					contains a new version of the pitch, with all the corrections, amendments and deletions suggested by Mr. Keogh (and/or Dr. Keogh)
 				</li>
 				<li>
+					This file&nbsp;
 					<a
 						href="https://drive.google.com/file/d/1ohQOhnSiVRi2QzfydBgoBBgXHT3IdppM/view?usp=sharing"
 						className="text-blue-600 underline"
 					>
 						<strong>CargoFlowDesign.pdf</strong>
 					</a>{" "}
-					- Evidence of our efforts to design our software.
+					shows evidence of our efforts to design our software.
 				</li>
 				<li>
+					This file&nbsp;
 					<a
 						href="https://drive.google.com/file/d/1Vs6hPF17p9imkeJorl4mx86eeUq6qeP2/view?usp=sharing"
 						className="text-blue-600 underline"
 					>
 						<strong>CargoFlowTesting.pdf</strong>
 					</a>{" "}
-					- Evidence of our efforts to test our software.
+					which shows evidence of our efforts to test our software.
 				</li>
 				<li>
-					This URL:{" "}
+					This URL{" "}
 					<a
 						href="https://github.com/pondohoo/cs-179-cargoflow"
 						className="text-blue-600 underline"
 					>
-						github.com/pondohoo/cs-179-cargoflow
+						<strong>github.com/pondohoo/cs-179-cargoflow</strong>
 					</a>{" "}
-					- Points to a GitHub repository of our code.
+					points to a GitHub repository of our code.
 				</li>
 				<li className="">
 					This{" "}
 					<a href="https://youtu.be/X93dlakq2KQ" className="text-blue-600 underline">
-						YouTube video
-					</a>{" "}
-					- Shows our software solving one balance and one transfer test case.
+						<strong>YouTube video</strong>
+					</a>
+					, that shows our software solving one balance, and one transfer test case (our choices, of the test cases Mr. Keogh gave us).
 				</li>
 				<li className="">
+					This text file, is the {" "}
 					<a href="https://drive.google.com/file/d/1iV4nN704zw3996auj11TexRCorT5U8I8/view?usp=sharing" className="text-blue-600 underline">
 						<strong>KeoghsPort2024.txt</strong>
-					</a>
-					 - Log created by our software
-					during the making of the YouTube video.
+					</a> {""}
+					log that was created by our software, during the making of the YouTube video in the previous bullet point.
 				</li>
 				<li className="">
 					We would like to point out the following limitations:
@@ -144,14 +133,24 @@ const Submission = () => {
 					</ul>
 				</li>
 				<li className="">
-					(EK says, this is optional) We would like to acknowledge the following ...
-					<ul className="list-disc list-inside">
-						<li className="">
-							As not all members were experts on Javascript and Next.js, we had to research and learn the language and framework.
-							The sources we consulted were: the Next.js documentation, the JavaScript documentation, the React documentation,
-							Google, Stack Overflow, and GeeksforGeeks.
-						</li>
-					</ul>
+					(EK says, this is optional) We would like to acknowledge the following resources that were helpful in completing this project.
+					As not all members were experts on Javascript and Next.js, we had to research and learn the language and framework.
+					The sources we consulted were: the{" "}
+					<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" className="text-blue-600 underline">
+						<strong>JavaScript documentation</strong>
+					</a>,{" "}
+					<a href="https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started" className="text-blue-600 underline">
+						<strong>React documentation</strong>
+					</a>,{" "}
+					<a href="https://www.google.com/" className="text-blue-600 underline">
+						<strong>Google</strong>
+					</a>,{" "}
+					<a href="https://stackoverflow.com/" className="text-blue-600 underline">
+						<strong>Stack Overflow</strong>
+					</a>,{" "}and{" "}
+					<a href="https://www.geeksforgeeks.org/" className="text-blue-600 underline">
+						<strong>GeeksforGeeks</strong>
+					</a>.
 				</li>
 			</ol>
 		</div>
